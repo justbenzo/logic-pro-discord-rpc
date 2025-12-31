b7940f4a9d522f390b02fcafd0b1053d08659830
justbenzo <henrymiller223@proton.me>
2025-12-31 13:01:52 -0600

    changed logiccheck from process name to bundle id to prevent false positives

---
+++ logiccheck.mm
@@ -0,0 +1,18 @@
+#include <libproc.h>
+#include <string.h>
+#include <iostream>
+
+bool is_logic_pro_running();
+
+// process_check.mm  (NOTE: must be .mm, not .cpp)
+#import <AppKit/NSRunningApplication.h>
+
+bool is_logic_pro_running() {
+    @autoreleasepool {
+        NSString* bundleId = @"com.apple.logic10";
+        NSArray<NSRunningApplication*>* apps =
+            [NSRunningApplication runningApplicationsWithBundleIdentifier:bundleId];
+        return (apps.count > 0);
+    }
+}
+
